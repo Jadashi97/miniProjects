532dd0942ea7e3b092111fff1f0f8b508bfb08dd
jadashi97 <njadauniversal23@yahoo.com>
2022-05-18 14:50:04 -0500

    (feat): figure why it wont find document by ID

--- whatsThePass/index.js
+++ whatsThePass/index.js
@@ -1,3 +1,5 @@
-let password = "lokose";
+let answer = document.getElementById("pword").innerHTML; //this grabs the password the user enters thru using ID
 
-console.log(password);
+let button = document.getElementById("btn") //this does what the action of the button which to submit thru ID
+
+console.log(answer);
