05efd5eb6a272f81b3744012159351778ac6a157
jadashi97 <njadauniversal23@yahoo.com>
2022-05-18 20:59:37 -0500

    (feat): add for loop and refactor code better so it can add decrease number of tries and show how many tries left

--- whatsThePass/index.js
+++ whatsThePass/index.js
@@ -1,5 +1,24 @@
-let answer = document.getElementById("pword").innerHTML; //this grabs the password the user enters thru using ID
+let pword = document.getElementById("pword"); //this grabs the password the user enters thru using ID
+let answer = document.getElementById("answer");
+let button = document.getElementById("btn");//this does what the action of the button which to submit thru ID
 
-let button = document.getElementById("btn") //this does what the action of the button which to submit thru ID
 
-console.log(answer);
+var password = "wordpass";
+
+var numberOfEntries = 0;
+
+var limitOfEntries = 5;
+
+button.addEventListener("click", function(){
+    if(numberOfEntries < limitOfEntries){
+        if(pword.value != password){
+            answer.innerHTML = "wrong Password, Please try Again!!!";
+            numberOfEntries ++;
+        }else{
+            numberOfEntries = 0;
+            answer.innerHTML = "Welcome Come on In!!"
+        }
+    }else{
+        answer.innerHTML = "Sorry Out of tries!!!"
+    }
+})
