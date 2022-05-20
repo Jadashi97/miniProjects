82ab61bcfd74e8ebf32ba88e749901c70fe72747
jadashi97 <njadauniversal23@yahoo.com>
2022-05-19 20:32:26 -0500

    (fix): figure out how to log the remaining tries left. (feat):add some css features

--- whatsThePass/index.js
+++ whatsThePass/index.js
@@ -3,19 +3,23 @@ let answer = document.getElementById("answer");
 let button = document.getElementById("btn");//this does what the action of the button which to submit thru ID
 
 
-var password = "wordpass";
+let password = "wordpass";
 
-var numberOfEntries = 0;
+let numberOfEntries = 0;
 
-var limitOfEntries = 5;
+let limitOfEntries = 5;
 
+
+//add event listen to run the call back function when butto is clicked
 button.addEventListener("click", function(){
     if(numberOfEntries < limitOfEntries){
         if(pword.value != password){
             answer.innerHTML = "wrong Password, Please try Again!!!";
+            console.log(pword.value);
             numberOfEntries ++;
         }else{
             numberOfEntries = 0;
+            //swaps the innerHtml to the string below
             answer.innerHTML = "Welcome Come on In!!"
         }
     }else{
