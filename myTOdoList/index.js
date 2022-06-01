949e3fc76a6362184adda2b468013493ebe453b3
jadashi97 <njadauniversal23@yahoo.com>
2022-06-01 11:26:20 -0500

    (feat): use form handler to grab the users input. add more functionality to include adding stuff todo list

--- myTOdoList/index.js
+++ myTOdoList/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const form = document.getElementById("add-to"); // grab the form thru the ID
+
+
+//this is the event handler to help grab input from the user
+function getFormInput(event){
+    event.preventDefault(); //this prevents the browser from executing the default submit behavior for a form
+    let input = document.getElementById("my-input").value;
+
+    if(input.value != " "){
+        console.log(input);
+    }else{
+        console.log("add a todo item!!");
+    }
+}
+
+//this is to add a selector 
+form.addEventListener("submit", getFormInput);
