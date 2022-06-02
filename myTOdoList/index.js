06f528a35d9076e2f1513cc546f625eff51740a0
jadashi97 <njadauniversal23@yahoo.com>
2022-06-02 10:43:45 -0500

    (feat): restructure all code to figure out how to parse the stuff. (fix): follow w3 to solve the issue

--- myTOdoList/index.js
+++ myTOdoList/index.js
@@ -1,10 +1,9 @@
-const form = document.getElementById("add-to"); // grab the form thru the ID
-
+const addbutton = document.getElementById("btn"); // grab the form thru the ID
 
 //this is the event handler to help grab input from the user
 function getFormInput(event){
     event.preventDefault(); //this prevents the browser from executing the default submit behavior for a form
-    let input = document.getElementById("my-input").value; //grabs value if the input
+    let input = document.getElementById("myInput").value; //grabs value if the input
 
     //logic behind what to add onto the list
     if(input.value != " "){
@@ -14,21 +13,22 @@ function getFormInput(event){
      };
 };
 
-//function to help handle the added item
+//function to add new item to do list when 
 function addtodo(todo){
     let ol = document.getElementById("my-ol"); //grabs the id element and assigns it to the var ul
     let li = document.createElement("li"); // creates a list element and assigns it to the var li
 
     //add span element to integrate into the todo list
     li.innerHTML = `
-    <span class="todo-item">${todo}</span> 
-    <button name ="inprogress">Doing</button>
-    <button name ="checkBtn">completed</button>
-    <button name ="deleteBtn">delete</button>
-
+    <span class="todo-item">${todo}</span>
+    <button name ="inprogress">👨🏾‍💻</button>
+    <button name ="checkBtn">✅</button>
+    <button name ="deleteBtn">🗑</button>
     `;
     ol.appendChild(li); // this adds the input to the ordered list of todos 
 
 };
+
 //this is to add a selector to handle what the should happen when the button is clicked
-form.addEventListener("submit", getFormInput);
+addbutton.addEventListener("submit", getFormInput);
+
