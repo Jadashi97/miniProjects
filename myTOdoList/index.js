7cd022643e0df3ef7d1e9a585e033e820d833128
jadashi97 <njadauniversal23@yahoo.com>
2022-06-03 11:01:24 -0500

    (fix): figure out the styling and finish off the behavior of the todo

--- myTOdoList/index.js
+++ myTOdoList/index.js
@@ -1,34 +1,30 @@
-const addbutton = document.getElementById("btn"); // grab the form thru the ID
 
-//this is the event handler to help grab input from the user
-function getFormInput(event){
-    event.preventDefault(); //this prevents the browser from executing the default submit behavior for a form
-    let input = document.getElementById("myInput").value; //grabs value if the input
+// Create a "close" button and append it to each list item
 
-    //logic behind what to add onto the list
-    if(input.value != " "){
-        addtodo(input)
-    }else{
-        input.value = " "; //this resets the input space to empty string after submitting
-     };
+let myList = document.getElementsByTagName("li") //grab the the list thru the Html tag name and set it as the variable
+
+for(let i = 0; i < myList.length; i++){ //for loop to go thru the entire mylist
+
+    var span = document.createElement("SPAN") //this creates a new HTML span element
+
+    var txt = document.createTextNode("\u00D7") //a method that creates a new text node with param as an X
+    span.className = "close";
+    span.appendChild(txt); // inserts the close text node
+
+    myList[i].appendChild(span); //inserts the span variable created in line 6  
 };
 
-//function to add new item to do list when 
-function addtodo(todo){
-    let ol = document.getElementById("my-ol"); //grabs the id element and assigns it to the var ul
-    let li = document.createElement("li"); // creates a list element and assigns it to the var li
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close"); //grab the element close and set it to variable close
 
-    //add span element to integrate into the todo list
-    li.innerHTML = `
-    <span class="todo-item">${todo}</span>
-    <button name ="inprogress">👨🏾‍💻</button>
-    <button name ="checkBtn">✅</button>
-    <button name ="deleteBtn">🗑</button>
-    `;
-    ol.appendChild(li); // this adds the input to the ordered list of todos 
+for(let i = 0; i < close.length; i++){ //loop thru the close 
+    close[i].onclick = function(){
+        var div = this.parentElement //set the element of the parent div to a var div
 
+        div.style.display = "none" //this toggles the display to not show much
+
+    }
 };
 
-//this is to add a selector to handle what the should happen when the button is clicked
-addbutton.addEventListener("submit", getFormInput);
+// Add a "checked" symbol when clicking on a list item
 
