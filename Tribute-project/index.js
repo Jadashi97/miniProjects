58419892f671e99c6d86baada9c2123a27ee49b6
jadashi97 <njadauniversal23@yahoo.com>
2022-06-18 12:38:26 -0500

    (feat): add styling and behavior to the tribute page, Image pops up on mouseover

--- Tribute-project/index.js
+++ Tribute-project/index.js
@@ -1,1 +1,10 @@
-console.log("Yaaaa ssaaaallaaammmm!!!!");
+//function to add what size the image should change on mouse hover
+function bigImg(x) {
+    x.style.height = "350px";
+    x.style.width = "300px";
+};
+  
+function normalImg(x) {
+    x.style.height = "230px";
+    x.style.width = "170px";
+};
