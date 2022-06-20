eb24939b2bb74ffa61441123ed73b4e1b822f092
jadashi97 <njadauniversal23@yahoo.com>
2022-06-20 10:17:44 -0500

    (feat):add styling to change the image color

--- Tribute-project/index.js
+++ Tribute-project/index.js
@@ -1,7 +1,7 @@
 //function to add what size the image should change on mouse hover
 function bigImg(x) {
-    x.style.height = "350px";
-    x.style.width = "300px";
+    x.style.height = "550px";
+    x.style.width = "400px";
 };
   
 function normalImg(x) {
