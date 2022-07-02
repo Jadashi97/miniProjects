0c82f2d590feb1f582dfaa822b51195e4de92c53
jadashi97 <njadauniversal23@yahoo.com>
2022-07-01 21:57:09 -0500

    figure out the create and read part of the app

--- contact-app/app.js
+++ contact-app/app.js
@@ -18,7 +18,7 @@ let contacts = [
         firstName: "Wani",
         lastName: "Ronald",
         otherName: "Jada",
-        birthData: 05 / 20 / 2000,
+        birthDate: 05 / 20 / 2000,
         phoneNumber: 0240034232,
         email: "ksdfkaj@yahoo.com",
         country: "USA",
@@ -37,15 +37,50 @@ const contactInfor = document.getElementById("contact-list");
 
 const ReadContactInfor = function (contacts) {
 
-    let initialContacts = "";
-    let i = 0;
+    let initialContacts = " ";
 
-    while (i < contacts.length) {
-        initialContacts += `<p id=${contacts[i].id}, ${contacts[i].firstName}, ${contacts[i].lastName}></p>`
-
-        i++
-
+    for(let i = 0;i <= contacts.length, i++;){
+        initialContacts += `<p data-id=${contacts[i].id}>${contacts[i].firstName}, ${contacts[i].lastName},
+        ${contacts[i].otherName}, ${contacts[i].birthDate}, ${contacts[i].phoneNumber}, ${contacts[i].birthDate},
+        ${contacts[i].email},${contacts[i].country},${contacts[i].city}, ${contacts[i].street},
+         ${contacts[i].state}></p>`
+        
     }
     return initialContacts;
+};
 
+
+//CREATE a new contact
+
+const CreateNewContact = function(event){
+    event.preventDefault();
+    const firstName = event.target["first-name"].value;
+    const lastName = event.target["last-name"].value;
+    const otherName = event.target["Oname"].value;
+    const birthDate = event.target["bday"].value;
+    const phoneNumber = event.target["phone"].value;
+
+    const newContactPerson = {
+
+        id: contacts.lenght + 1,
+        firstName: "Lokose",
+        lastName: "Daniel",
+        otherName: "Nyarji",
+        birthData: 08/10/1997,
+        phoneNumber: 0240034232,
+        email: "ksdfkaj@gmail.com",
+        country: "USA",
+        city: "Minneapolis",
+        street: 4000,
+        state: "MN"
+
+    };
+
+    contacts.push(newContactPerson);
+
+    contactInfor.innerHTML = ReadContactInfor(contacts);
 }
+
+const contactForm = document.querySelector("#create-new-contact");
+
+contactForm.addEventListener("submit", CreateNewContact);
