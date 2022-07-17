cfd423fb80200c2dadabfcecbdbc5d6f4d11e996
jadashi97 <njadauniversal23@yahoo.com>
2022-07-17 16:38:01 -0400

    (feat): Internalize the CRUD operation  using vanilla js to manipulate data

--- contact-app/app.js
+++ contact-app/app.js
@@ -27,84 +27,92 @@ let contacts = [
         state: "MN"
     }
 
-]
+];
 
 
 //get handle of the contact list
 
 const contactInfor = document.getElementById("contact-list");
-contactInfor.innerHTML = contacts;
+// contactInfor.innerHTML = contacts.id;
 
 
 
 
 //the CRUD list view 
 
-const readContactInfor = function(contacts) {
-
-    let initialContacts = " ";
+const readContactInfor = function(contacts,index, arr) {
+    console.log(contacts);
+    // let initialContacts = " ";
     
-    i = 0;
+    // i = 0;
 
-    while(i < contacts.length){
-        initialContacts += `<p data-id=${contacts[i].id}, ${contacts[i].firstName}, ${contacts[i].lastName},
-        ${contacts[i].phoneNumber},
-        ${contacts[i].email},${contacts[i].country},${contacts[i].city},${contacts[i].state}
-        </p>`
-        i++;
-    };
-    return initialContacts;
+    // while(i < contacts.length){
+    //     initialContacts += `<p data-id=${contacts[index].id}, ${contacts[index].firstName}, ${contacts[index].lastName},
+    //     ${contacts[index].phoneNumber},
+    //     ${contacts[index].email},${contacts[index].country},${contacts[index].city},${contacts[index].state}
+    //     </p>`
+    //     i++;
+    // };
+    // return initialContacts;
     // console.log(initialContacts)
     // contactInfor.innerHTML = initialContacts;
 };
 
+let readList = contacts.map(readContactInfor);
+
+// console.log(readList);
+
+let addedContacts = contacts.push(readList)
+console.log(addedContacts);
+
+
 //CREATE a new contact
 
-const createNewContact = function(event){
-    event.preventDefault();
-    const firstName = event.target["first-name"].value;
-    const lastName = event.target["last-name"].value;
-    // const otherName = event.target["Oname"].value;
-    // const birthDate = event.target["bday"].value;
-    const phoneNumber = event.target["phone"].value;
+// const createNewContact = function(event){
+//     event.preventDefault();
+//     const firstName = event.target["first-name"].value;
+//     const lastName = event.target["last-name"].value;
+//     // const otherName = event.target["Oname"].value;
+//     // const birthDate = event.target["bday"].value;
+//     const phoneNumber = event.target["phone"].value;
 
-    const newContactPerson = {
+//     const newContactPerson = {
 
-        id: contacts.length + 1,
-        firstName: "Lokose",
-        lastName: "Daniel",
-        otherName: "Nyarji",
-        birthData: 08/10/1997,
-        phoneNumber: 0240034232,
-        email: "ksdfkaj@gmail.com",
-        country: "USA",
-        city: "Minneapolis",
-        street: 4000,
-        state: "MN"
+//         id: contacts.length + 1,
+//         firstName: "Lokose",
+//         lastName: "Daniel",
+//         otherName: "Nyarji",
+//         birthData: 08/10/1997,
+//         phoneNumber: 0240034232,
+//         email: "ksdfkaj@gmail.com",
+//         country: "USA",
+//         city: "Minneapolis",
+//         street: 4000,
+//         state: "MN"
 
-    };
+//     };
 
-    contacts.push(newContactPerson);
-    contactInfor.innerHTML = readContactInfor(contacts);
+//     // contacts.push(newContactPerson);
+//     // contactInfor.innerHTML = readContactInfor(contacts);
 
-}
-
-
-const contactForm = document.querySelector("#create-new-contact");
-
-contactForm.addEventListener("submit", createNewContact);
+// }
 
 
-const populateContactForm = (id) => {
-    document.getElementById("first-name").value = contacts[id].firstName;
-	document.getElementById("last-name").value = contacts[id].lastName;
+// const contactForm = document.querySelector("#create-new-contact");
+
+// contactForm.addEventListener("submit", createNewContact);
 
 
-    const createBtn = document.getElementById("create");
-}
+// const populateContactForm = (id) => {
+//     document.getElementById("first-name").value = contacts[id].firstName;
+// 	document.getElementById("last-name").value = contacts[id].lastName;
 
 
-document.addEventListener("DOMContentLoaded", () => {
-	contactInfor.innerHTML = readContactInfor(contacts);
-});
+//     const createBtn = document.getElementById("create");
+// }
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+// 	contactInfor.innerHTML = readContactInfor(contacts);
+// });
 
