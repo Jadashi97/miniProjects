92270ef1f01bbb11be97ee998fbd94e157e92055
jadashi97 <njadauniversal23@yahoo.com>
2022-08-10 22:38:09 -0500

    (fix): finish off calc 2.0

--- Calculator-2.0/index.js
+++ Calculator-2.0/index.js
@@ -1,8 +1,26 @@
 let form = document.getElementById("form-result");
+let result = document.getElementById("calc-result");
 
 
-console.log(form);
+
+function processForm(event){
+
+    event.preventDefault();
+
+    let num1 = parseInt(document.getElementById("n1").value);
+    let num2 = parseInt(document.getElementById("n2").value);
+    let operator = document.getElementById("operator").value;
+
+    result.innerHTML = `Result: ${num1}`
 
 
-// process form 
+}
+
+
+
+
+// process form //////pay attention more to this and make sure you internalize it.
+form.addEventListener("submit", processForm);
+
+
 
