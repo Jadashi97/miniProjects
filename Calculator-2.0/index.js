a70b4ebe7d953a747ee5eea2c9c51a1f0cf82356
jadashi97 <njadauniversal23@yahoo.com>
2022-08-12 10:26:45 -0500

    (feat): calc 2.0 functionality done. add CSS features to create the UI beautiful

--- Calculator-2.0/index.js
+++ Calculator-2.0/index.js
@@ -1,6 +1,22 @@
 let form = document.getElementById("form-result");
 let result = document.getElementById("calc-result");
 
+const add = (num1, num2)=> {
+    return num1 + num2
+};
+
+const subtract = (num1, num2)=> {
+    return num1 - num2
+};
+
+const multiply = (num1, num2)=> {
+    return num1 * num2
+};
+
+const divide = (num1, num2)=> {
+    return num1 / num2
+};
+
 
 
 function processForm(event){
@@ -11,25 +27,25 @@ function processForm(event){
     let num2 = parseInt(document.getElementById("n2").value);
     let operator = document.getElementById("operator").value;
 
-    result.innerHTML = `Result: ${computeResult(num1, num2, operator)}`
+    result.innerHTML = `Result: ${computeResult(num1, operator, num2)}`
 }
 
 function computeResult(num1, operator, num2){
-    if(operator = "+"){
-        return num1 + num2;
+    if(operator === "+"){
+        return add(num1, num2);
 
-    } else if(operator = "-"){
-        return num1 - num2;
+    } else if(operator === "-"){
+        return subtract(num1, num2);
 
-    }else if(operator = "*"){
-        return num1 * num2;
+    }else if(operator === "*"){
+        return multiply(num1, num2);
 
-    }else if(operator = "/"){
-        return num1 / num2;
+    }else if(operator === "/"){
+        return divide(num1,  num2);
     }
 }
 
-console.log("2", "+", "4");
+// console.log(add(2, 4));
 
 
 
