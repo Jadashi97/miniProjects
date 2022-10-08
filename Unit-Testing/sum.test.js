204fcec035d51e9c340c639bad1f3bf237d6a14e
jadashi97 <njadauniversal23@yahoo.com>
2022-10-08 09:05:17 -0500

    (feat): learned more testing truthniness using jest

--- Unit-Testing/sum.test.js
+++ Unit-Testing/sum.test.js
@@ -1,20 +1,20 @@
 const sum = require('./sum');
 
 ////////// TESTING EXACT QUALITY/////////////
-// test('adds 1 + 2  to equal 3', ()=>{
-//     expect(sum(2,1)).toBe(3);
-// }); 
+test('adds 1 + 2  to equal 3', ()=>{
+    expect(sum(2,1)).toBe(3);
+}); 
 
-// test("two plus three is five", ()=>{
-//     expect(sum(2,3)).toBe(5);
-// });
+test("two plus three is five", ()=>{
+    expect(sum(2,3)).toBe(5);
+});
 
 ///////// TESTING OBJECTS ////////////////////
-// test('Object assignment', ()=>{
-//     const data = {one: 1};
-//     data['two'] = 4;
-//     expect(data).toEqual({one: 1, two: 4});
-// });
+test('Object assignment', ()=>{
+    const data = {one: 1};
+    data['two'] = 4;
+    expect(data).toEqual({one: 1, two: 4});
+});
 
 test('adding positive numbers is not zero', () => {
     for(let a = 1; a < 10; a++){
@@ -22,4 +22,22 @@ test('adding positive numbers is not zero', () => {
             expect(a + b).not.toBe(0);
         }
     }
+})
+
+///////////Truthiness///////////
+test('null', ()=>{
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+})
+test('zero', ()=>{
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
 })
