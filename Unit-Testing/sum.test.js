72919d30fbbcf23437991715e28b8bedf64d9684
jadashi97 <njadauniversal23@yahoo.com>
2022-10-09 21:39:15 -0500

    (feat): learned numbers and strings testing  using jest

--- Unit-Testing/sum.test.js
+++ Unit-Testing/sum.test.js
@@ -1,6 +1,6 @@
 const sum = require('./sum');
 
-////////// TESTING EXACT QUALITY/////////////
+// ////////// TESTING EXACT QUALITY/////////////
 test('adds 1 + 2  to equal 3', ()=>{
     expect(sum(2,1)).toBe(3);
 }); 
@@ -9,7 +9,7 @@ test("two plus three is five", ()=>{
     expect(sum(2,3)).toBe(5);
 });
 
-///////// TESTING OBJECTS ////////////////////
+// ///////// TESTING OBJECTS ////////////////////
 test('Object assignment', ()=>{
     const data = {one: 1};
     data['two'] = 4;
@@ -24,7 +24,7 @@ test('adding positive numbers is not zero', () => {
     }
 })
 
-///////////Truthiness///////////
+// ///////////Truthiness///////////
 test('null', ()=>{
     const n = null;
     expect(n).toBeNull();
@@ -40,4 +40,34 @@ test('zero', ()=>{
     expect(z).not.toBeUndefined();
     expect(z).not.toBeTruthy();
     expect(z).toBeFalsy();
+})
+
+// // ///////////Numbers///////////
+test('two plus two', ()=> {
+    const value = 3 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(6);
+    expect(value).toBeLessThanOrEqual(4.5);
+
+    //toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+
+})
+
+test('adding floating point numbers', () =>{
+    const value = 0.1 + 0.2;
+    // expect(value).toBe(0.3);
+
+    expect(value).toBeCloseTo(0.3);
+})
+
+// // ///////////Strings///////////
+test('there is no W in am', () => {
+    expect('team').not.toMatch(/I/)
+})
+
+test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/)
 })
