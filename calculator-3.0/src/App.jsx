2ac2e578382cb4744e19bb354434b1c2decb9eb2
jadashi97 <njadauniversal23@yahoo.com>
2023-01-02 18:16:25 -0600

    (feat): learned hooks and added to app functionality

--- calculator-3.0/src/App.jsx
+++ calculator-3.0/src/App.jsx
@@ -1,25 +1,47 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css';
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  const plus = (e)=> {
+    e.preventDefault()
+    setResult((result)=> result + Number(inputRef.current.value))
+  }
+
+
+  const resetInput = (e)=>{
+    e.preventDefault()
+    inputRef.current.value = 0;
+  }
+
+  const resetResult = (e)=> {
+    e.preventDefault()
+    setResult((prevVal)=> prevVal * 0)
+  }
+
 
   return (
     <div className="App">
       <h1>Calculator 3.0</h1>
       <form action="">
-        <label>
-          Input Number:
-        </label>
-        <input/>
-        <br/>
-        <label htmlFor="operator">Operator</label>
+        <p>{result}</p>
+        <input
+          pattern='[0-9]'
+          ref={inputRef}
+          type='number'
+          placeholder='Type number'
+        />
+        <br />
+        <button>add</button>
+        <button onClick={resetResult}>reset Result</button>
+        <button>{resetInput}</button>
 
       </form>
-
-    
     </div>
   )
 }
