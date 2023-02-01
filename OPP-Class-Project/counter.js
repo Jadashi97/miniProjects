bf18fa62b173085aa74e35dc625bd8b58daf76eb
jadashi97 <njadauniversal23@yahoo.com>
2023-02-01 09:08:07 -0600

    (fix): going in depth to learn about classes in OOP

---
+++ OPP-Class-Project/counter.js
@@ -0,0 +1,48 @@
+class Animal {
+    constructor(color= "yellow", energy = 100){
+      this.color = color;
+      this.energy = energy;
+    }
+    isActive(){
+      if(this.energy > 0){
+        this.energy -= 20;
+        console.log('Energy is decreasing currently at:', this.energy)
+      }else if(this.energy == 0){
+        this.sleep()
+      }
+    }
+    sleep(){
+      this.energy += 20;
+      console.log('Energy is increasing, currently at:', this.energy);
+    }
+    getColor(){
+     console.log(this.color)
+    }
+}
+
+class Cat extends Animal{
+  constructor(sound="purr", canJumpHigh= true, canClimbTrees = true, color, energy){
+    super(color, energy);
+    this.sound = sound;
+    this.canClimbTrees = canClimbTrees;
+    this.canJumpHigh = canJumpHigh;
+  }
+  makeSound(){
+    console.log(this.sound);
+  }
+}
+
+class Bird extends Animal(){
+  constructor(sound='quaks', canFly= true, color, energy){
+    super(color, energy);
+    this.sound = sound;
+    this.canFly = canFly;
+  }
+  makeSound(){
+    console.log(this.sound);
+  }
+}
+
+class HouseCat extends Animal(){
+
+}
