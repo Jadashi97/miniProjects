4a7ad83fcfeea3c2b0d4afc7b90fd30a241eae62
jadashi97 <njadauniversal23@yahoo.com>
2023-02-19 21:38:56 -0600

    (fix):add js functionality to detect when button is clicked

---
+++ rotateWordsCss/rotate.js
@@ -0,0 +1,22 @@
+var  word1 = document.querySelectorAll("letters").length;
+
+for(var i = 0; i < word1; i++){
+    document.querySelectorAll(".letters")[i].addEventListener("click", function(){
+
+        var buttonInnterHtml  = this.innerHTML;
+        handleClick(buttonInnterHtml);
+    })
+}
+
+
+// detect when button is clicked
+
+document.addEventListener("keydown", function(){
+    handleClick();
+
+})
+
+function handleClick(){
+    console.log("I have been clicked!!");
+}
+
